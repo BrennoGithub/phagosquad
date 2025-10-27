d1fb10c3ad28195b86d7f7c08f7cf5ef05b0ccc7
Júlio César <o.cesar@escolar.ifrn.edu.br>
2025-10-27 13:20:04 +0000

    Adição de texto na página inicial

--- js/roteamento.js
+++ js/roteamento.js
@@ -1,69 +1,66 @@
 const inicial = `
-    <!-- CONTEÚDO -->
     <div class="overlay"></div>
-    <div class="video-container">
-        <video controls>
-            <source src="videos/projeto.mp4" type="video/mp4">
-        </video>
+    <div class="sobre_video video-container">
+        <div class="head_video head">
+            <h1>Conheça os Leucócitos</h1>
+        </div>
+        <div class="body_video">
+            <video controls>
+                <source src="videos/projeto.mp4" type="video/mp4">
+            </video>
+            <p>
+                Os <span class="linkSessao">leucócitos</span>, também conhecidos como glóbulos brancos, são células do sangue responsáveis 
+                pela defesa do organismo. Fazem parte do sistema imunológico, combatendo microrganismos invasores, 
+                toxinas e células anormais. São fundamentais para a imunidade e para manter o corpo livre de infecções.
+                Visto isso, os leucócitos realizam dois tipos de processos no processo de defesa do organismo, que são a <span class="linkSessao">endocitose</span>,
+                quando a célula engole partículas do meio externo, permitindo com que os leucócitos absorvam e destruam microrganismos, 
+                restos celulares ou substâncias tóxicas, e a <span class="linkSessao">diapedese</span>, quando os leucócitos saem da corrente sanguínea e migram para tecidos
+                ou órgãos, atravessando as paredes dos capilares, geralmente em resposta a um estímulo inflamatório ou lesão tecidual. 
+            <p/>
+        </div>
     </div>
-
-    <!-- FUNDO -->
     <div class="background-gif"></div>`;
 
 const sobre = `
-    <div>
-        <div class="overlay"></div>
-        <div class="sobre_grupo">
-            <div class="head">
-                <h1>Conheça a Phagosquad</h1>
-            </div>
-            <div class="body">
-                <p>
-                    A <span class="sessao linkSessao">Phagosquad</span> é o grupo que apresentará o trabalho sobre a relação entre
-                    Leucócitos, Endocitose e Diapedese. Somos do Info 3V do campus Parnamirim e ficamos com a responsabilidade
-                    de produzir uma <span class="sessao linkSessao">Biomaket</span> e este lindíssimo site.
-                </p>
-                    <br>
-                <p>
-                    Os leucócitos, também conhecidos como glóbulos brancos, são células do sangue responsáveis pela 
-                    defesa do organismo. Fazem parte do sistema imunológico, combatendo microrganismos invasores, 
-                    toxinas e células anormais. São fundamentais para a imunidade e para manter o corpo livre de 
-                    infecções. Seu processo é conhecido como Endocitose, quando a célula engole partículas do meio externo, 
-                    envolvendo sua membrana plasmática Este mecanismo de transporte de massa se divide em fagocitose 
-                    (englobar partículas sólidas grandes), pinocitose (ingestão de fluidos e partículas pequenas) 
-                    e endocitose mediada por receptor (captação específica de moléculas).
-                <p/>
-                <img src="imgs/Phagosquad_logo.ico" alt="Logo Phagosquad">
-            </div>
+    <div class="overlay"></div>
+    <div class="sobre_grupo">
+        <div class="head">
+            <h1>Conheça a Phagosquad</h1>
+        </div>
+        <div class="body">
+            <img src="imgs/Phagosquad_logo.ico" alt="Logo Phagosquad">
+            <p>
+                A <span class="sessao linkSessao">Phagosquad</span> é o grupo que apresentará o trabalho sobre a relação entre
+                Leucócitos, Endocitose e Diapedese. Somos do Info 3V do campus Parnamirim e ficamos com a responsabilidade
+                de produzir uma <span class="sessao linkSessao">Biomaket</span> e este lindíssimo site.
+            </p>
         </div>
     </div>
-    <div class="background-gif-grupo"></div>`; //Ajeitar <a href="#" class="sessao"><strong>Biomaket</strong></a>
+    <div class="background-gif-grupo"></div>`;
 
 const materiais = `
-    <div>
-        <div class="overlay"></div>
-        <div class="sobre_grupo">
-            <div class="head">
-                <h1>Lista de Materiais</h1>
-            </div>
-            <div class="body">
-                <p></p>
-                <ol id="esquerda">
-                    <li class="itemLista">Papelão grosso (base)</li>
-                    <li class="itemLista">Papel alumínio</li>
-                    <li class="itemLista">Fitas vermelhos</li>
-                    <li class="itemLista">Massa de modelar branca, roxa e verde</li>
-                    <li class="itemLista">Palito de churrasco</li>
-                    <li class="itemLista">Papel/placas</li>
-                    <li class="itemLista">Adesivos</li>
-                    <li class="itemLista">Cola quente</li>
-                    <li class="itemLista">Tesoura/estilete</li>
-                    <li class="itemLista">Tinta</li>
-                    <li class="itemLista">Papel Machê</li>
-                    <li class="itemLista">Veniz</li>
-                    <li class="itemLista">Ímas</li>                                        
-                </ol>
-            </div>
+    <div class="overlay"></div>
+    <div class="sobre_grupo">
+        <div class="head">
+            <h1>Lista de Materiais</h1>
+        </div>
+        <div class="body">
+            <p></p>
+            <ol id="esquerda">
+                <li class="itemLista">Papelão grosso (base)</li>
+                <li class="itemLista">Papel alumínio</li>
+                <li class="itemLista">Fitas vermelhos</li>
+                <li class="itemLista">Massa de modelar branca, roxa e verde</li>
+                <li class="itemLista">Palito de churrasco</li>
+                <li class="itemLista">Papel/placas</li>
+                <li class="itemLista">Adesivos</li>
+                <li class="itemLista">Cola quente</li>
+                <li class="itemLista">Tesoura/estilete</li>
+                <li class="itemLista">Tinta</li>
+                <li class="itemLista">Papel Machê</li>
+                <li class="itemLista">Veniz</li>
+                <li class="itemLista">Ímas</li>                                        
+            </ol>
         </div>
     </div>
     <div class="background-gif-materiais"></div>`;
@@ -72,7 +69,7 @@ const biomarket = `
     <div class="overlay"></div>
     <div class="sobre_grupo">
         <div class="head">
-            <h1>Bio-Maket</h1>
+            <h1>Biomaket</h1>
         </div>
         <div class="body">
             <p></p>
@@ -106,23 +103,24 @@ async function Roteamento(titulo, pagina){
     switch(titulo){
         case "Sobre":
             pagina.innerHTML = sobre;
-            await Sessoes(titulo);
+            await Sessoes();
             break;
         case "Biomaket":
             pagina.innerHTML = biomarket;
+            const sessoes = document.querySelectorAll(".sessao");
             sessoes[2].style.fontWeight = "bold";
             sessoes[2].style.backgroundColor = "#FF3F33";
-            await Sessoes(titulo);
+            await Sessoes();
             break;
         case "Materiais":
             pagina.innerHTML = materiais;
-            await Sessoes(titulo);
+            await Sessoes();
             break;
         default:
             pagina.innerHTML = inicial
             document.querySelector(".sessao").style.fontWeight = "bold";
             document.querySelector(".sessao").style.backgroundColor = "#FF3F33";
-            await Sessoes(titulo);
+            await Sessoes();
             break;
     }
 }
@@ -131,5 +129,5 @@ const pagina = document.getElementById("conteudoPagina");
 
 document.addEventListener("DOMContentLoaded", async () => {
     await Roteamento("", pagina); 
-    Sessoes();
+    await Sessoes();
 })
